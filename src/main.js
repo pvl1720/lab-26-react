10b5d7301a01b817c2399eb61d04bc969009268f
Maks <pvl1720@gmail.com>
2018-01-12 23:03:36 -0800

    commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-//
+import React from 'react';
+import ReactDom from 'react-dom';
+import superagent from 'superagent';
+import {say} from 'cowsay';
+import './style/main.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+
+}
+
+ReactDom.render(<App/>,document.getElementById('root'));
