f2df090e0876858e117030acd8a7b1e689b6d556
Maks <pvl1720@gmail.com>
2018-01-14 17:42:53 -0800

    cosway1

--- src/main.js
+++ src/main.js
@@ -2,15 +2,30 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import superagent from 'superagent';
 import {say} from 'cowsay';
+import faker from 'faker';
 import './style/main.scss';
 
-class App extends React.Component {
+import { think, SQUIRREL } from 'cowsay';
+
+
+class Header extends React.Component {
+
   render() {
+
     return (
-      <div>Hello World</div>
+      <header>
+        <h1>Generate Cowsay Lorem</h1>
+        <button type="button">Click Me!</button>
+      </header>
     )
   }
-
 }
 
-ReactDom.render(<App/>,document.getElementById('root'));
+// class App extends React.Component {
+//
+//     constructor(props) {
+//     }
+// }
+
+
+ReactDom.render(<Header/>,document.getElementById('root'));
