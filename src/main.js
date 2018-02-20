1ebb68d9f233f1c9b5ae09d86b2b38411a80e4cd
Maks <pvl1720@gmail.com>
2018-02-19 22:24:55 -0800

    old code need to finish

--- src/main.js
+++ src/main.js
@@ -7,25 +7,65 @@ import './style/main.scss';
 
 import { think, SQUIRREL } from 'cowsay';
 
+console.log(think({
+  text: 'chillin in da browser',
+  cow: SQUIRREL,
+  eyes: 'DD',
+  tongue: ';;',
+}));
+
 
 class Header extends React.Component {
 
-  render() {
+  constructor(props){
+     super(props);
+   }
 
+  render() {
     return (
       <header>
-        <h1>Generate Cowsay Lorem</h1>
-        <button type="button">Click Me!</button>
+        <h1>Generate Cowsay Lorem </h1>
+        // <button type="button">Click Button!</button>
       </header>
     )
   }
+
 }
 
-// class App extends React.Component {
-//
-//     constructor(props) {
-//     }
-// }
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          content: 'I\'m a cow, not a squirrel!',
+          cow: SQUIRREL
+        };
+
+        this.handleCow = this.handleCow.bind(this);
+        // this.updateState = this.updateState.bind(this);
+    }
+
+    handleCow(e) {
+      // console.log('handle cow click me button');
+      const content = faker.fake('{{lorem.sentence}}');
+      this.setState({content: Lorem.paragraph(2, true)});
+    }
 
 
-ReactDom.render(<Header/>,document.getElementById('root'));
+    render() {
+        return (
+            <div>
+                <Header />
+                <div id="root">
+                  <button onClick={this.handleCow}>Click Button!</button>
+                  <pre>
+                {say({ text: this.state.content})}
+              </pre>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(<App/>, document.getElementById('root'));
